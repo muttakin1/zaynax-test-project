0894b5f6bac5b1be67c567ca3382f16d15367b31
Muttakin Islam <islammuttakin@live.com>
2020-11-19 18:23:21 +0600

    deployable app

--- config/config.js
+++ config/config.js
@@ -2,11 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/Zaynax-DB'
+  mongoUri: "mongodb+srv://Muttakin Islam:Faluda12@cluster0.xrfwe.mongodb.net/ZAYNAX-TEST-PROJECT?retryWrites=true&w=majority"
 }
 
 export default config
